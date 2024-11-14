782cd806be71b7cc7b23751bcee569e325f82407
JonasKucinskas <jonaskucinskas2@gmail.com>
2024-11-14 18:39:13 +0200

    testing setup

---
+++ client/src/test/java/com/javakaian/YourClassTest.java
@@ -0,0 +1,12 @@
+package com.javakaian;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class YourClassTest {
+    @Test
+    public void testMethod() {
+        int result = 5;  // Example
+        assertEquals(5, result);
+    }
+}
